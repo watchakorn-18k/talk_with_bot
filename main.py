b774f0d4ad8a65d790e7a3467cbb41e58edbe554
watchakorn-18k <porton2559@gmail.com>
2023-06-15 03:13:58 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,303 @@
+import flet as ft
+import datetime
+
+
+def create_state(name, value=None):
+    globals()[name] = value
+
+
+class InputField(ft.UserControl):
+    def __init__(self, page):
+        super().__init__()
+        self.text_field = ft.TextField(
+            height=50,
+            width=300,
+            border_color=ft.colors.PRIMARY,
+            on_change=self.on_change_text,
+        )
+        self.page = page
+        self.button_send = ft.IconButton(
+            icon=ft.icons.SEND,
+            disabled=True,
+        )
+
+    def update(func):
+        def wrapper(self, *args, **kwargs):
+            func(self, *args, **kwargs)
+            super().update()
+            self.page.update()
+
+    def on_change_text(self, e):
+        self.button_send.disabled = False if e.control.value else True
+        super().update()
+        self.page.update()
+
+    def build(self):
+        return ft.Container(
+            content=ft.Row(
+                [
+                    self.text_field,
+                    ft.Container(
+                        self.button_send,
+                        bgcolor=ft.colors.PRIMARY_CONTAINER,
+                        border_radius=10,
+                        height=50,
+                    ),
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            )
+        )
+
+
+class MainPage(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+        self.list_view = ft.ListView(height=500)
+        self.text_show_from_input = "สวัสดีจ้า"
+
+    def build(self):
+        data_example = [
+            {
+                "id": "ตัวอย่าง",
+                "content": [
+                    "ไอเดียสร้างสรรค์สำหรับวันเกิดอายุ 10 ขวบไหม?",
+                    "ฉันจะส่งคำขอ HTTP ใน Javascript ได้อย่างไร",
+                    "อธิบายการคำนวณควอนตัมด้วยคำศัพท์ง่ายๆ",
+                ],
+            },
+            {
+                "id": "ความสามารถ",
+                "content": [
+                    "สามารถถามได้ทุกอย่าง หากตอบไม่ได้จะทำการปฏิเสธ",
+                    "ได้รับการฝึกฝนให้ปฏิเสธคำขอที่ไม่เหมาะสม",
+                    "ใช้งานได้ฟรี ไม่จำเป็นต้องใช้ token ในการใช้งานก่อน",
+                ],
+            },
+            {
+                "id": "ข้อจำกัด",
+                "content": [
+                    "บางครั้งอาจสร้างคำแนะนำที่เป็นอันตรายหรือเนื้อหาที่มีอคติ",
+                    "ความรู้จำกัดของโลกและเหตุการณ์หลังปี 2021",
+                    "บางครั้งอาจสร้างข้อมูลที่ไม่ถูกต้อง",
+                ],
+            },
+        ]
+        header_example = ft.Row(alignment=ft.MainAxisAlignment.SPACE_AROUND)
+        exaple_list = ft.Row(alignment=ft.MainAxisAlignment.SPACE_AROUND)
+
+        for i in data_example:
+            header_example.controls.append(
+                ft.Row(
+                    [
+                        ft.Text(i["id"]),
+                    ],
+                )
+            )
+            exaple_list.controls.append(
+                ft.Column(
+                    [
+                        ft.Card(
+                            content=ft.Container(
+                                ft.Text(
+                                    i["content"][0],
+                                    text_align=ft.TextAlign.CENTER,
+                                    size=10,
+                                ),
+                                width=87,
+                                padding=ft.padding.all(10),
+                            ),
+                            height=85,
+                        ),
+                        ft.Card(
+                            content=ft.Container(
+                                ft.Text(
+                                    i["content"][1],
+                                    text_align=ft.TextAlign.CENTER,
+                                    size=10,
+                                ),
+                                width=87,
+                                padding=ft.padding.all(10),
+                            ),
+                            height=85,
+                        ),
+                        ft.Card(
+                            content=ft.Container(
+                                ft.Text(
+                                    i["content"][2],
+                                    text_align=ft.TextAlign.CENTER,
+                                    size=10,
+                                ),
+                                width=87,
+                                padding=ft.padding.all(10),
+                            ),
+                            height=85,
+                        ),
+                    ],
+                )
+            )
+
+        controls = ft.Column(
+            [
+                ft.Text("พูดคุยกับ AI", size=30, color=ft.colors.PRIMARY),
+                ft.Container(padding=5),
+                ft.Row(
+                    [
+                        ft.Icon(name="person", size=30, color=ft.colors.PRIMARY),
+                        ft.Icon(name="bolt", size=30, color=ft.colors.PRIMARY),
+                        ft.Icon(name="smart_toy", size=30, color=ft.colors.PRIMARY),
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                ),
+                ft.Container(padding=5),
+                ft.Column(
+                    [header_example, exaple_list],
+                ),
+                ft.Container(height=80),
+            ]
+        )
+
+        return controls
+
+    def chat_page(self):
+        self.client_card = ft.Row(
+            [
+                ft.Icon(name="person", color=ft.colors.PRIMARY, size=30),
+                ft.Column(
+                    [
+                        ft.Row(
+                            [
+                                ft.Text(
+                                    f"{datetime.datetime.now().strftime('%d/%m/%Y')},{datetime.datetime.now().strftime('%H:%M:%S')}",
+                                    size=10,
+                                ),
+                            ]
+                        ),
+                        ft.Card(
+                            content=ft.Container(
+                                content=ft.Column(
+                                    [
+                                        ft.Text(self.text_show_from_input),
+                                    ]
+                                ),
+                                width=300,
+                                padding=20,
+                            )
+                        ),
+                    ]
+                ),
+            ]
+        )
+        self.bot_card = ft.Row(
+            [
+                ft.Icon(name="smart_toy", color=ft.colors.SECONDARY, size=30),
+                ft.Column(
+                    [
+                        ft.Row(
+                            [
+                                ft.Text(
+                                    f"{datetime.datetime.now().strftime('%d/%m/%Y')},{datetime.datetime.now().strftime('%H:%M:%S')}",
+                                    size=10,
+                                ),
+                            ]
+                        ),
+                        ft.Card(
+                            content=ft.Container(
+                                content=ft.Column(
+                                    [
+                                        ft.Text(self.text_show_from_input),
+                                    ]
+                                ),
+                                width=300,
+                                padding=20,
+                            )
+                        ),
+                    ]
+                ),
+            ]
+        )
+
+        self.list_view.controls = [self.client_card, self.bot_card]
+
+        return ft.Column(
+            [
+                self.list_view,
+                ft.Container(height=30),
+            ]
+        )
+
+
+class AppTalkBot(ft.UserControl):
+    def __init__(self, page):
+        super().__init__()
+        self.page = page
+        self.app_bar()
+        self.input_text = InputField(self.page)
+        self.main_page = MainPage()
+        self.text_field = self.input_text.text_field
+        self.btn_send = self.input_text.button_send
+        self.btn_send.on_click = self.after_click
+
+    # create decoration
+    def update(func):
+        def wrapper(self, *args, **kwargs):
+            func(self, *args, **kwargs)
+            super().update()
+            self.page.update()
+
+        return wrapper
+
+    @update
+    def switch_theme(self, e):
+        self.page.theme_mode = (
+            ft.ThemeMode.LIGHT
+            if self.page.theme_mode == ft.ThemeMode.DARK
+            else ft.ThemeMode.DARK
+        )
+        super().update()
+
+    @update
+    def app_bar(self):
+        button_switch = ft.IconButton(
+            icon=ft.icons.DARK_MODE, on_click=self.switch_theme
+        )
+        self.page.appbar = ft.AppBar(
+            leading_width=40,
+            title=ft.Text("คุยกับ AI"),
+            center_title=False,
+            bgcolor=ft.colors.PRIMARY_CONTAINER,
+            actions=[button_switch],
+        )
+
+    @update
+    def after_click(self, e):
+        if self.text_field.value != "":
+            self.main_page.text_show_from_input = self.text_field.value
+            self.main_page.controls.pop()
+            self.main_page.controls.append(self.main_page.chat_page())
+            self.main_page.update()
+        else:
+            pass
+
+    def build(self):
+        return ft.Container(
+            content=ft.Column(
+                [
+                    self.main_page,
+                    self.input_text,
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+            ),
+            padding=30,
+        )
+
+
+def main(page: ft.Page):
+    page.title = "คุยกับ AI"
+    page.theme = ft.Theme(color_scheme_seed=ft.colors.INDIGO)
+    app = AppTalkBot(page)
+    page.window_height = 1039
+    page.window_width = 575
+    page.add(app)
+
+
+ft.app(main, view=ft.WEB_BROWSER, port=8080)
---
+++ readme.md
@@ -0,0 +1,73 @@
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://cdn.discordapp.com/attachments/585069498986397707/1112788994175012936/image-removebg-preview.png">
+  <img src="https://cdn.discordapp.com/attachments/585069498986397707/1112788501642104832/OIG.png">
+</picture>
+
+<h1 align="center">talk_with_bot</h1>
+<p align="center">Talk with AI GPT</p>
+
+<p align="center">
+  <a href="LICENSE" target="_blank">
+    <img alt="Python" src="https://img.shields.io/badge/version-0.1-gree?style=for-the-badge&logoColor=white&logo=Python" />
+  </a>
+
+</p>
+
+## Description
+
+Talk with AI GPT
+
+## Getting Started
+
+```
+git clone https://github.com/watchakorn-18k/talk_with_bot.git
+
+cd talk_with_bot
+
+```
+
+## Installation
+
+```
+# create virtualenv auto name
+fenv env
+
+# install package in requirements.txt
+fenv install
+
+# Or use pip
+pip install -r requirements.txt
+
+```
+
+## Usage
+
+Instructions on how to use the project, including any usage examples and screenshots.
+
+## Tree
+
+<!--- Start Tree --->
+
+```bash
+.
+└── talk_with_bot/
+        └──.vscode/
+                └──settings.json
+        └──env_talk_with_bot/
+        └──.env
+        └──.gitignore
+        └──main.py
+        └──readme.md
+        └──requirements.txt
+
+```
+
+<!--- End Tree --->
+
+## Contributing
+
+If you would like to contribute to the project, include a section on how to do so, including any guidelines and best practices.
+
+## License
+
+Include information about the license used for the project, such as the name of the license (e.g. MIT, Apache 2.0, etc.) and a link to the license text.
